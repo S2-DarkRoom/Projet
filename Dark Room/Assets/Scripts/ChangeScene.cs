f0c396429112e1fe1578406d6c66ac33bcaaeee8
Emma <leroy.emma@live.fr>
2018-03-07 13:04:18 +0100

    Ajout d'écrans titre

---
+++ Dark Room/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour {
+
+	void Start () {
+		
+	}
+
+	void Update () 
+	{
+        Scene sc = SceneManager.GetActiveScene();
+
+		if (Input.GetKeyDown (KeyCode.Space)) 
+		{
+            if (sc.name == "TitleGame")
+                SceneManager.LoadScene("TitleCredit");
+            else if (sc.name == "TitleCredit")
+                    SceneManager.LoadScene("Menu");
+            else if (sc.name == "Menu")
+                SceneManager.LoadScene("Game");
+        }
+	}
+}
