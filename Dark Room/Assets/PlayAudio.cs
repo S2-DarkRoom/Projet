6ca053b7feebe378a4f65263267f4d7f25342919
Emma <leroy.emma@live.fr>
2018-04-29 16:37:30 +0200

    Beginning of cinematic and pickup icons

---
+++ Dark Room/Assets/PlayAudio.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAudio : MonoBehaviour {
+
+    void Start()
+    {
+        AudioSource audio = GetComponent<AudioSource>();
+        audio.Play();
+    }
+}
