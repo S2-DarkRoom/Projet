fc3954013e1d55ce24cba304e9cc651bc4450bea
Low_battery <marile.lin@epita.fr>
2018-04-29 16:38:25 +0200

    multi à fixer
    qqs corrections mais il reste encore des choses à fixer.

--- Dark Room/Assets/Scripts/ChangeScene.cs
+++ Dark Room/Assets/Scripts/ChangeScene.cs
@@ -5,10 +5,32 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour {
 
-	void Start () {
-		
-	}
+	void Start ()
+    {
+        Scene sc = SceneManager.GetActiveScene();
 
+        if (sc.name == "TitleGame")
+        {
+            StartCoroutine(Wait());
+            SceneManager.LoadScene("TitleCredit");
+        }
+            
+        else if (sc.name == "TitleCredit")
+        {
+            StartCoroutine(Wait());
+            SceneManager.LoadScene("Menu");
+        }
+            
+        else if (sc.name == "Menu")
+            SceneManager.LoadScene("Game");
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSecondsRealtime(20);
+    }
+
+    /*
 	void Update () 
 	{
         Scene sc = SceneManager.GetActiveScene();
@@ -23,4 +45,7 @@ public class ChangeScene : MonoBehaviour {
                 SceneManager.LoadScene("Game");
         }
 	}
+    */
+
+
 }
--- Dark Room/Assets/Scripts/PlayerController.cs
+++ Dark Room/Assets/Scripts/PlayerController.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController :  MonoBehaviour
-{
+public class PlayerController : MonoBehaviour {
 
     public float speed;
 	
