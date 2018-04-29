ba2ade3c08b683cda9d68ea183a79c28891b602b
Low_battery <marile.lin@epita.fr>
2018-04-29 03:14:49 +0200

    multi à modif
    Ajout d'un nouveau script avec qq modif et des erreurs en moins. A merge aussi

---
+++ Dark Room/Assets/PlayerSetup.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using unityEngine.Network;
+
+public class PlayerSetup : NetworkBehaviour
+{ 
+    [SerializeField]
+    Behaviour[] componentsToDisable;
+
+    private void Start()
+    {
+        if (!isPlayerController)
+        {
+            for (int i = 0; i < componentsToDisable.Length; i++)
+            {
+                componentsToDisable[i].enabled = false;
+            }
+        }
+
+        else
+        {
+            Camera.main.gameObject.SetActive(false);
+        }
+    }
+}
