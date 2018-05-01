ee00163af0106cc32f70b49b70b54a2be05b9a55
Low_battery <marile.lin@epita.fr>
2018-05-01 17:47:20 +0200

    Animations des portes ok !
    Reprendre la scène avec les décos !

--- Dark Room/Assets/Scripts/Inventory.cs
+++ Dark Room/Assets/Scripts/Inventory.cs
@@ -1,9 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Networking;
 
-public class Inventory : NetworkBehaviour
+public class Inventory : MonoBehaviour
 {
     #region Singleton
     public delegate void OnItemChanged();
---
+++ Dark Room/Assets/Scripts/InventoryUI_multi.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class InventoryUI_multi : NetworkBehaviour
+{
+
+    public Transform itemsParent;
+    public GameObject inventoryUI;
+    Inventory inventory;
+
+    InventorySlot[] slots;
+    void Start()
+    {
+        inventory = Inventory.instance;
+
+        slots = itemsParent.GetComponentsInChildren<InventorySlot>();
+        inventoryUI.SetActive(!inventoryUI.activeSelf);
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            inventoryUI.SetActive(!inventoryUI.activeSelf);
+        }
+    }
+
+    void UpdateUI()
+    {
+
+        for (int i = 0; i < slots.Length; i++)
+        {
+            if (i < Inventory.items.Count)
+            {
+                slots[i].AddItem(Inventory.items[i]);
+            }
+        }
+
+    }
+}
