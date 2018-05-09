6f07640601d38cbec4db1b60ac321b66ad2e5c88
Emma <leroy.emma@live.fr>
2018-05-09 19:44:28 +0200

    Updated script
    Working until AddDigits()
    Digits still not showing (Update?)

--- Dark Room/Assets/Scripts/CodeUI.cs
+++ Dark Room/Assets/Scripts/CodeUI.cs
@@ -7,10 +7,15 @@ public class CodeUI : MonoBehaviour {
     public GameObject UI;
     public string code;
     string entered = "";
+    int curr;
+    public Transform digitsParent;
+
+    DigitSlot[] slots;
 
     private void Start()
     {
         UI.SetActive(false);
+        slots = digitsParent.GetComponentsInChildren<DigitSlot>();
     }
 
     private void Update()
@@ -34,6 +39,84 @@ public class CodeUI : MonoBehaviour {
             {
                 return;
             }
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad0) || Input.GetKeyDown(KeyCode.Keypad1) || Input.GetKeyDown(KeyCode.Keypad2) || Input.GetKeyDown(KeyCode.Keypad3) || Input.GetKeyDown(KeyCode.Keypad4) || Input.GetKeyDown(KeyCode.Keypad5) || Input.GetKeyDown(KeyCode.Keypad6) || Input.GetKeyDown(KeyCode.Keypad7) || Input.GetKeyDown(KeyCode.Keypad8) || Input.GetKeyDown(KeyCode.Keypad9))
+        {
+            Debug.Log("Entered a digit");
+
+            if (Input.GetKeyDown(KeyCode.Keypad0))
+            {
+                entered += "0";
+                curr = 0;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Keypad1))
+            {
+                Debug.Log("Entered 1");
+                entered += "1";
+                curr = 1;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Keypad2))
+            {
+                entered += "2";
+                curr = 2;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Keypad3))
+            {
+                entered += "3";
+                curr = 3;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Keypad4))
+            {
+                entered += "4";
+                curr = 4;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Keypad5))
+            {
+                entered += "5";
+                curr = 5;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Keypad6))
+            {
+                entered += "6";
+                curr = 6;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Keypad7))
+            {
+                entered += "7";
+                curr = 7;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Keypad8))
+            {
+                entered += "8";
+                curr = 8;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Keypad9))
+            {
+                entered += "9";
+                curr = 9;
+            }
+
+            for (int i = 0; i < slots.Length; i++)
+            {
+                if (i < entered.Length)
+                {
+                    slots[i].AddDigit(curr);
+                }
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Delete) || Input.GetKeyDown(KeyCode.Backspace))
+        {
+
         }
     }
 
---
+++ Dark Room/Assets/Scripts/DigitSlot.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DigitSlot : MonoBehaviour
+{
+    public Image icon;
+    public Sprite[] digits = new Sprite[10];
+
+    public void AddDigit(int i)
+    {
+        Debug.Log("add a digit in position" + i);
+        icon.sprite = digits[i];
+        icon.enabled = true;
+    }
+
+    public void DeleteDigit()
+    {
+        icon.enabled = false;
+    }
+}
