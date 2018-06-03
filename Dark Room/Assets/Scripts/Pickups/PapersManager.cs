6d3136f3255a2f76b52297f8374716aae357e6db
Emma <leroy.emma@live.fr>
2018-06-03 23:46:58 +0200

    Fixed PapersManager and modified portal

--- Dark Room/Assets/Scripts/Pickups/PapersManager.cs
+++ Dark Room/Assets/Scripts/Pickups/PapersManager.cs
@@ -6,27 +6,21 @@ using UnityEngine.UI;
 public class PapersManager : MonoBehaviour
 {
     bool FR;
+    public Image sheet;
 
     public void Show(Paper paper)
     {
-        GetComponentInChildren<Image>().sprite = paper.sprite;
-        GameObject.Find("Player").GetComponent<PlayerController>().enabled = false;
-        GameObject.Find("Player").GetComponentInChildren<FirstPerson>().enabled = false;
+        sheet.sprite = paper.sprite;
     }
 
     public void Update()
     {
-        FR = FindObjectOfType<SettingsManager>().FR;
         if (Input.GetKeyDown(KeyCode.X))
-        {
-            GameObject.Find("Player").GetComponent<PlayerController>().enabled = true;
-            GameObject.Find("Player").GetComponentInChildren<FirstPerson>().enabled = true;
             gameObject.SetActive(false);
-        }
     }
 
     public void OnGUI()
     {
-        GUI.Label(new Rect(Screen.width / 2 - 20, Screen.height / 0.95f, 200f, 200f), FR? "[X] Fermer" : "[X] Close");
+        GUI.Label(new Rect(Screen.width / 2 - 20, Screen.height * 0.9f, 200f, 200f), FR? "[X] Fermer": "[X] Close");
     }
 }
