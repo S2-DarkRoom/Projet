7ec31461bc8b63e920b5d8776fb20e947914e37c
Low_battery <marile.lin@epita.fr>
2018-06-04 14:26:24 +0200

    Stop au Handspinner
    FAITS : Fix bug + ajouts et modifs de plusieurs scripts + ajout du son. 
    IMPORTANTS ET A MODIFIER : Le script du minuteur + inventaire qui n'affiche pas les pickups + lampe torche impossible à récup.
    A AJOUTER : ajout des nouvelles décos.

---
+++ Dark Room/Assets/Scripts/Minuteur_multi.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class Minuteur_multi : NetworkBehaviour
+{
+    [SyncVar] public float gameTime = 3600.0f; //The length of a game, in seconds.
+    [SyncVar] public float timer; //How long the game has been running. -1=waiting for players, -2=game is done
+    [SyncVar] public int minPlayers = 2; //Number of players required for the game to start
+    [SyncVar] public bool masterTimer = false; //Is this the master timer?
+                                               //public ServerTimer timerObj;
+
+    Minuteur_multi serverTimer;
+
+    void Start()
+    {
+        if (isServer)
+        { // For the host to do: use the timer and control the time.
+            if (isLocalPlayer)
+            {
+                serverTimer = this;
+                masterTimer = true;
+            }
+        }
+        else if (isLocalPlayer)
+        { //For all the boring old clients to do: get the host's timer.
+            Minuteur_multi[] timers = FindObjectsOfType<Minuteur_multi>();
+            for (int i = 0; i < timers.Length; i++)
+            {
+                if (timers[i].masterTimer)
+                {
+                    serverTimer = timers[i];
+                }
+            }
+        }
+    }
+    void Update()
+    {
+        if (masterTimer)
+        { //Only the MASTER timer controls the time
+            if (timer >= gameTime)
+            {
+                timer = -2;
+            }
+            else if (timer == -1)
+            {
+                if (NetworkServer.connections.Count >= minPlayers)
+                {
+                    timer = 0;
+                }
+            }
+            else if (timer == -2)
+            {
+                //Game done.
+                SceneManager.LoadScene("Menu");
+            }
+            else
+            {
+                timer += Time.deltaTime;
+            }
+        }
+
+        if (isLocalPlayer)
+        { //EVERYBODY updates their own time accordingly.
+            if (serverTimer)
+            {
+                gameTime = serverTimer.gameTime;
+                timer = serverTimer.timer;
+                minPlayers = serverTimer.minPlayers;
+            }
+            else
+            { //Maybe we don't have it yet?
+                Minuteur_multi[] timers = FindObjectsOfType<Minuteur_multi>();
+                for (int i = 0; i < timers.Length; i++)
+                {
+                    if (timers[i].masterTimer)
+                    {
+                        serverTimer = timers[i];
+                    }
+                }
+            }
+        }
+    }
+
+    void OnGUI()
+    {
+        GUI.Box(new Rect(20, 20, 75, 20), "0 : " + ((gameTime / 60) % 60 - 1).ToString("0") + " : " + (gameTime % 60).ToString("0"));
+    }
+}
---
+++ Dark Room/Assets/Scripts/RayCast_multi.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class RayCast_multi : NetworkBehaviour {
+
+    public GameObject flashlight;
+    public float distance;
+    RaycastHit hit;
+    float displayTime = 2;
+    bool displayMessage = false;
+    string message = "";
+    bool lockedMessage = false;
+
+    void Update ()
+    {
+        Debug.DrawRay(this.transform.position, this.transform.forward * distance, Color.magenta);
+        if (Physics.Raycast(this.transform.position, this.transform.forward, out hit, distance))
+        {
+            if (hit.collider.tag == "pickup")
+            {
+                displayMessage = true;
+                message = "[E] Ramasser";
+
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    Pickups item = hit.collider.GetComponent<Pickups>();
+                    if (item.name == "Flashlight")
+                    {
+                        FindObjectOfType<AudioManager>().Play("Flashlight");
+                        flashlight.GetComponent<flashlight>().Enabled();
+                    }
+
+                    if (item.name.Substring(0, 4) == "Door" || item.name == "Chest")
+                        FindObjectOfType<AudioManager>().Play("Key");
+
+                    Inventory.instance.Add(item);
+                    displayMessage = false;
+
+                    Destroy(hit.collider.gameObject);
+                }
+            }
+
+
+            else if (hit.collider.tag == "doors")
+            {
+                Doors door = hit.collider.GetComponent<Doors>();
+
+                if (lockedMessage)
+                {
+                    displayTime -= Time.deltaTime;
+
+                    if (displayTime <= 0.0)
+                    {
+                        lockedMessage = false;
+                        displayMessage = false;
+                        displayTime = 2;
+                    }
+                }
+
+                else if (door.open == false)
+                {
+                    displayMessage = true;
+                    message = "[E] Ouvrir";
+
+                    if (Input.GetKeyDown(KeyCode.E))
+                    {
+                        if (door.TryOpen() == false)
+                        {
+                            displayMessage = true;
+                            message = "Locked";
+                            lockedMessage = true;
+                        }
+
+                        else
+                        {
+                            door.open = true;
+                            displayMessage = false;
+                        }
+                    }
+                }
+
+                /* A UPDATE : POSSIBILITE DE FERMER PORTE OU MEUBLE
+                else if (name != "Door1" && name != "Door2" && name != "Door3" && name != "DoorToilet3" && name != "DoorToilet2" && name != "DoorToilet1" && name != "DoorSecret")
+                {
+                    displayMessage = true;
+                    message = "[E] Fermer";
+                    if (Input.GetKeyDown(KeyCode.E))
+                    {
+                        door.Close();
+                    }
+                }
+                */
+            }
+            
+            else
+            {
+                displayMessage = false;
+                lockedMessage = false;
+            }
+            
+        }
+    }
+
+    void OnGUI()
+    {
+        if (displayMessage)
+        {
+            GUI.Label(new Rect(Screen.width / 2 - 20, Screen.height / 2 + 50, 200f, 200f), message);
+        }
+    }
+}
