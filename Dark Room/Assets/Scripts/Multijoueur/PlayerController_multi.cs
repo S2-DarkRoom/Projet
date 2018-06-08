0efbad381ff8e099c45d3773858ce6d7d9fc2aff
Low_battery <marile.lin@epita.fr>
2018-06-08 21:57:59 +0200

    Animations
    A FAIRE IMPORTANTS : La rotation du player qui fait le 360° sur y + le script du raycast qu'il faut adapter en multi (donc comprend le pb de la lampe torche, coffre fort, le papier cloué à la poutre de la room1) + menu qui s'affiche pas quand on appuie sur échap + les objets ramassés qui ne s'affichent pas dans l'inventaire + et autres (listés prochainement sur GitHub). 
    A FAIRE MAIS RAPIDE : ajout des nouvelles décos rajoutés par Emma dans la scène Game et synchroniser des animations s'il y en a.

--- Dark Room/Assets/Scripts/Multijoueur/PlayerController_multi.cs
+++ Dark Room/Assets/Scripts/Multijoueur/PlayerController_multi.cs
@@ -17,26 +17,29 @@ public class PlayerController_multi : MonoBehaviour
 
     private void Update()
     {
-        float _xMov = Input.GetAxisRaw("Horizontal");
-        float _zMov = Input.GetAxisRaw("Vertical");
+        if (!Pausemenu.paused)
+        {
+            float _xMov = Input.GetAxisRaw("Horizontal");
+            float _zMov = Input.GetAxisRaw("Vertical");
 
-        Vector3 _moveHorizontal = transform.right * _xMov;
-        Vector3 _moveVertical = transform.forward * _zMov;
+            Vector3 _moveHorizontal = transform.right * _xMov;
+            Vector3 _moveVertical = transform.forward * _zMov;
 
-        Vector3 _velocity = (_moveHorizontal + _moveVertical).normalized * speed;
+            Vector3 _velocity = (_moveHorizontal + _moveVertical).normalized * speed;
 
-        motor.Move(_velocity);
+            motor.Move(_velocity);
 
-        float _yRot = Input.GetAxisRaw("Mouse X");
+            float _yRot = Input.GetAxisRaw("Mouse X");
 
-        Vector3 _rotation = new Vector3(0, _yRot, 0) * lookSensitivity;
+            Vector3 _rotation = new Vector3(0, _yRot, 0) * lookSensitivity;
 
-        motor.Rotate(_rotation);
+            motor.Rotate(_rotation);
 
-        float _xRot = Input.GetAxisRaw("Mouse Y");
+            float _xRot = Input.GetAxisRaw("Mouse Y");
 
-        Vector3 _cameraRotation = new Vector3(_xRot, 0, 0) * lookSensitivity;
+            Vector3 _cameraRotation = new Vector3(_xRot, 0, 0) * lookSensitivity;
 
-        motor.RotateCamera(_cameraRotation);
+            motor.RotateCamera(_cameraRotation);
+        }
     }
  }
