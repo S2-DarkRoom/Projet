77cd755fe2db5e8d8c455e5297b1a21104d90bb5
Emma <leroy.emma@live.fr>
2018-06-10 15:25:18 +0200

    Bonne gestion de l'End Game
    * Chronomètre se stoppe à 00 :00 :00
    * UI s'active
    * UI fonctionne

--- Dark Room/Assets/Scripts/Player/Minuteur.cs
+++ Dark Room/Assets/Scripts/Player/Minuteur.cs
@@ -7,6 +7,7 @@ using UnityEngine.UI;
 
 public class Minuteur : MonoBehaviour
 {
+    private bool end = false;
     public GameObject mainCam, endCam;
     public GameObject UIend;
 
@@ -14,10 +15,11 @@ public class Minuteur : MonoBehaviour
 
     void Update ()
     {
-        time -= Time.deltaTime; 
+        time -= Time.deltaTime * 200; 
 
 		if (time <= 0) 
 		{
+            end = true;
             EndGame();
 		}
 	}
@@ -26,16 +28,25 @@ public class Minuteur : MonoBehaviour
     {
         mainCam.SetActive(false);
         endCam.SetActive(true);
-        endCam.GetComponent<Animator>().SetBool("end", true);
+        //endCam.GetComponent<Animator>().SetBool("end", true);
         UIend.SetActive(true);
     }
 
     void OnGUI()
     {
-        TimeSpan t = TimeSpan.FromSeconds(time);
-        GUI.Box(new Rect(20, 20, 75, 20), string.Format("{0:D2} : {1:D2} : {2:D2}",
-                t.Hours,
-                t.Minutes,
-                t.Seconds));
+        if (!end)
+        {
+            TimeSpan t = TimeSpan.FromSeconds(time);
+            GUI.Box(new Rect(20, 20, 75, 20), string.Format("{0:D2} : {1:D2} : {2:D2}",
+                    t.Hours,
+                    t.Minutes,
+                    t.Seconds));
+        }
+
+        else
+        {
+            GUI.color = Color.red;
+            GUI.Box(new Rect(20, 20, 75, 20), "00 : 00 : 00");
+        }
     }
 }
