fc7bbdb928bb40e3f0a7eaf88807e9b7ec0de957
Emma <leroy.emma@live.fr>
2018-06-11 19:56:50 +0200

    Cinematic : added UI and Jennefer's voice

--- Dark Room/Assets/Scripts/LastCinematic.cs
+++ Dark Room/Assets/Scripts/LastCinematic.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Video;
 
 public class LastCinematic : MonoBehaviour
@@ -8,25 +9,40 @@ public class LastCinematic : MonoBehaviour
     private int i = 0;
     public Material[] mats;
     public GameObject screen;
+    public Image UI;
+    public Sprite n1EN, n2EN, n2FR, n1FR;
+    private bool FR;
 
     public void Play()
     {
+        FR = FindObjectOfType<SettingsManager>().FR;
+        UI.enabled = false;
         FindObjectOfType<AudioManager>().Play("Outro");
     }
 
     public void ChangeScreen()
     {
-        if (i == 0)
-            screen.GetComponent<VideoPlayer>().enabled = true;
-
-        else if (i == 1)
-            screen.GetComponent<VideoPlayer>().enabled = false;
-
-        if (i != 5)
-            screen.GetComponent<Renderer>().material = mats[i];
-
-        else if (i == 4)
-            screen.GetComponent<VideoPlayer>().enabled = true;
+        switch (i)
+        {
+            case (0):
+                screen.GetComponent<VideoPlayer>().enabled = true;
+                screen.GetComponent<Renderer>().material = mats[i];
+                break;
+            case (1):
+                screen.GetComponent<VideoPlayer>().enabled = false;
+                screen.GetComponent<Renderer>().material = mats[i];
+                break;
+            case (2):
+                UI.enabled = true;
+                UI.sprite = FR ? n1FR : n1EN;
+                break;
+            case (3):
+                
+            case (4):
+                UI.enabled = false;
+                screen.GetComponent<VideoPlayer>().enabled = true;
+                break;
+        }
 
         i++;
     }
