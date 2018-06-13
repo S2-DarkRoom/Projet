c1c6d710a634600fe7f7a923685b8859c02790bd
Emma <leroy.emma@live.fr>
2018-06-13 22:11:01 +0200

    Fixing stuff

---
+++ Dark Room/Assets/Connect.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class Connect : MonoBehaviour
+{
+    NetworkManager nm;
+    bool prev = true;
+
+    void Start()
+    {
+        nm = gameObject.GetComponent<NetworkManager>();
+    }
+
+    void Update()
+    {
+        if (prev != nm.isNetworkActive)
+        {
+            prev = nm.isNetworkActive;
+
+            if (prev)
+            {
+                Cursor.visible = false;
+                Cursor.lockState = CursorLockMode.Locked;
+            }
+            else
+            {
+                Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.None;
+            }
+        }
+    }
+}
